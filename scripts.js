71580a65275d172917d08002bf6386dd17592696
Christiano Viana <christiano_viana@hotmail.com>
2025-10-07 14:43:20 -0300

    Add files via upload

--- scripts.js
+++ scripts.js
@@ -12,5 +12,21 @@ function toggleTheme() {
   document.body.classList.toggle("dark-mode");
 }
 
+let currentIndex = 0;
+
+function moveCarousel(direction) {
+  const track = document.querySelector(".carousel-track");
+  const cards = document.querySelectorAll(".publication-card");
+  const totalCards = cards.length;
+
+  currentIndex += direction;
+
+  if (currentIndex < 0) currentIndex = 0;
+  if (currentIndex >= totalCards) currentIndex = totalCards - 1;
+
+  const offset = -currentIndex * 100;
+  track.style.transform = `translateX(${offset}%)`;
+}
+
 fetch("https://vitn0kfkej.execute-api.us-east-1.amazonaws.com/registrar?origem=portfolio")
 
