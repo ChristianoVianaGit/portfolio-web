8280bef6641fe738eb17991c0f1d9e2c69bfa7f7
Christiano Viana <christiano_viana@hotmail.com>
2025-10-08 00:38:45 -0300

    Add files via upload

--- scripts.js
+++ scripts.js
@@ -28,5 +28,32 @@ function moveCarousel(direction) {
   track.style.transform = `translateX(${offset}%)`;
 }
 
-fetch("https://vitn0kfkej.execute-api.us-east-1.amazonaws.com/registrar?origem=portfolio")
+function abrirModal(id) {
+  document.getElementById(id).style.display = "block";
+}
 
+function fecharModal(id) {
+  const modal = document.getElementById(id);
+  const conteudo = modal.querySelector(".modal-conteudo");
+
+  conteudo.classList.add("fechando");
+  modal.classList.add("fechando");
+
+  setTimeout(() => {
+    modal.style.display = "none";
+    conteudo.classList.remove("fechando");
+    modal.classList.remove("fechando");
+  }, 300); // tempo igual ao da animação
+}
+
+// Fecha o modal ao clicar fora dele
+window.onclick = function(event) {
+  const modais = document.querySelectorAll(".modal");
+  modais.forEach(modal => {
+    if (event.target === modal) {
+      modal.style.display = "none";
+    }
+  });
+};
+
+fetch("https://vitn0kfkej.execute-api.us-east-1.amazonaws.com/registrar?origem=portfolio")
